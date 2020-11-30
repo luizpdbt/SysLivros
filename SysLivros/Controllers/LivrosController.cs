50364b1ab86da08664d728765615fcdfb9a3f4ec
Luiz Francisco <Luiz.uapiba@hotmail.com>
2020-11-30 19:30:03 -0300

    Adicionar arquivos de projeto.

---
+++ SysLivros/Controllers/LivrosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SysLivros.Controllers
+{
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("api/Livros")]
+        public IActionResult RetornaLivros()
+        {
+
+            return Ok(new 
+            {
+                livros="Cronnicas de Narnia,Fantastico Mundo de Bob,Mulher gato"
+            });
+        }
+
+
+    }
+}
